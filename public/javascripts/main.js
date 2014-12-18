99c5f1a453b2cc141d5fed346ba4fa9f1b60e6cc
CodingHouse <sb@codinghouse.co>
2014-12-17 17:02:05 -0800

    Cleanup after santos

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,3 @@
-// navigator.geolocation.getCurrentPosition(function(position) {
-//   var latitude = position.coords.latitude;
-//   var longitude = position.coords.longitude;
-//   console.log(latitude, longitude);
-// });
-
 angular.module('AB', [])
 .controller('MainController', ['$http', '$scope', function($http, $scope) {
 }]);
@@ -12,28 +6,25 @@ var map;
 var service;
 var infowindow;
 
-function initialize() {
+function fetchPlaces() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var current = new google.maps.LatLng(latitude,longitude);
 
- navigator.geolocation.getCurrentPosition(function(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  var current = new google.maps.LatLng(latitude,longitude);
+    map = new google.maps.Map(document.getElementById('map'), { center: current, zoom: 15 });
 
-  map = new google.maps.Map(document.getElementById('map'), { center: current, zoom: 15 });
+    var request = {
+      location: current,
+      radius: '1600',
+      types: ['amusement_park', 'aquarium', 'art_gallery', 'bar', 'book_store', 'bowling_alley', 'cafe', 'casino', 'food', 'library', 'museum', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa', 'zoo']
+    };
 
-  var request = {
-    location: current,
-    radius: '1600',
-    types: ['amusement_park', 'aquarium', 'art_gallery', 'bar', 'book_store', 'bowling_alley', 'cafe', 'casino', 'food', 'library', 'museum', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa', 'zoo']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, function(results, status) {
+      debugger;
+    });
   });
 }
 
-function callback(results, status) {
-  debugger;
-}
-
-initialize();
+fetchPlaces();

dd6c71b8436f1d59f722d6c9cd4dd09f4e51db71
CodingHouse <sb@codinghouse.co>
2014-12-17 17:11:47 -0800

    get location on load

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,23 @@
 angular.module('AB', [])
 .controller('MainController', ['$http', '$scope', function($http, $scope) {
-}]);
-
-var map;
-var service;
-var infowindow;
-
-function fetchPlaces() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var current = new google.maps.LatLng(latitude,longitude);
-
+    $scope.latitude = position.coords.latitude;
+    $scope.longitude = position.coords.longitude;
+  });
+  var map;
+  var service;
+  var infowindow;
+  $scope.fetchPlaces = function() {
+    var current = new google.maps.LatLng($scope.latitude, $scope.longitude);
     map = new google.maps.Map(document.getElementById('map'), { center: current, zoom: 15 });
-
     var request = {
       location: current,
       radius: '1600',
       types: ['amusement_park', 'aquarium', 'art_gallery', 'bar', 'book_store', 'bowling_alley', 'cafe', 'casino', 'food', 'library', 'museum', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa', 'zoo']
     };
-
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, function(results, status) {
       debugger;
     });
-  });
-}
-
-fetchPlaces();
+  };
+}]);
